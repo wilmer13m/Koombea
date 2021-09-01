ff81a30c1bd3b53ddbc22ac62d8568f29bcaa621
wilmer mendoza <wilmer.mendoza@elitgon.com>
2021-09-01 18:44:20 -0400

    Development Wilmer Mendoza: Se deshabilito la interacion del usuario con los imageviews miestras se estan descargando  las imagenes de internet

--- Koombea Post/Koombea Post/Utils/Extensions/UIImageView+Extension.swift
+++ Koombea Post/Koombea Post/Utils/Extensions/UIImageView+Extension.swift
@@ -11,12 +11,21 @@ import Kingfisher
 extension UIImageView {
     
     func loadImageFrom(url: String) {
-        
+        self.isUserInteractionEnabled = false
         self.tintColor = .darkGray
         self.contentMode = .scaleAspectFill
         let url = URL(string: url)
         self.kf.indicatorType = .activity
-        self.kf.setImage(with: url, placeholder: UIImage(systemName: "photo.fill"))
+//        self.kf.setImage(with: url, placeholder: UIImage(systemName: "photo.fill"))
+        self.kf.setImage(with: url, placeholder: UIImage(systemName: "photo.fill"), options: nil) { (result) in
+            print(result)
+            switch result {
+            case .success:
+                self.isUserInteractionEnabled = true
+            default:
+                break
+            }
+        }
     }
 }
 
